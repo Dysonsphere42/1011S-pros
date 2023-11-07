1d935b88d0076b254d80b8417882e7d07be559ec
Dysonsphere042 <dysonsphere42@proton.me>
2023-11-07 08:27:05 -0800

    imported lemlib library

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "lemlib/api.hpp"
 
 /**
  * A callback function for LLEMU's center button.
@@ -54,7 +55,9 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous(){
+	
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
